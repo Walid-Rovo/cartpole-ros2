96f3b2a5049ff37d740bb962424938ab85f52b87
Walid Ismail <walid.msi@outlook.com>
2023-03-12 15:13:42 +0100

    Add bridged joint and force topics

--- launch/cartpole_ign_bringup.launch.py
+++ launch/cartpole_ign_bringup.launch.py
@@ -1,71 +1,90 @@
 import os
-
 import xacro
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # Get pkg path
-    pkg_cartpole_01 = get_package_share_directory("cartpole_01")
+    # Package Directories
+    pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
+    pkg_cartpole_01 = get_package_share_directory('cartpole_01')
 
-    # Ignition gazebo env variable to make pkg folders discoverable by it
-    if "IGN_GAZEBO_RESOURCE_PATH" in os.environ:
-        model_path = os.environ["IGN_GAZEBO_RESOURCE_PATH"] + ":" + pkg_cartpole_01
+    # Parse robot description from xacro
+    robot_description_file =  os.path.join(pkg_cartpole_01, 'urdf', 'cartpole.xacro')
+    robot_description_config = xacro.process_file(
+        robot_description_file
+    )
+    robot_description = {"robot_description": robot_description_config.toxml()}
+
+    # Robot state publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+
+    # Ignition gazebo
+    if 'IGN_GAZEBO_RESOURCE_PATH' in os.environ:
+        model_path = os.environ['IGN_GAZEBO_RESOURCE_PATH'] + ':' + pkg_cartpole_01
     else:
         model_path = pkg_cartpole_01
 
-    # Robot description (cartpole.xacro -> xml)
-    robot_description_file = os.path.join(pkg_cartpole_01, "urdf", "cartpole.xacro")
-    robot_description_xacro = xacro.process_file(robot_description_file)
-    robot_description = robot_description_xacro.toxml()
-
-    # Robot state publisher
-    params = {'use_sim_time': True, 'robot_description': robot_description}
-    robot_state_publisher =  Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[params],
-            arguments=[])
-
-    # Ignition gazebo
-    pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py')),
-        launch_arguments={'ign_args': '-r sdf/cartpole_empty.sdf'}.items(),
+        launch_arguments={'ign_args': '-r empty.sdf'}.items(),
     )
 
-    # Spawner
-    spawner = Node(package='ros_ign_gazebo', executable='create',
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_cartpole_01, 'rviz', 'joint_states.rviz')],
+    )
+
+    # Spawn
+    spawn = Node(package='ros_ign_gazebo', executable='create',
                  arguments=[
-                    # '-world', 'cartpole_empty_world',
-                    '-name', 'cartpole',
-                    '-topic', '/robot_description'],
-                 output='screen')
+                     '-name', 'cartpole',
+                     '-topic', 'robot_description',
+                 ],
+                 output='screen',
+                 )
 
-    # # Bridge
-    # bridge = Node(
-    #     package="ros_ign_bridge",
-    #     executable="parameter_bridge",
-    #     arguments=[
-    #         "/world/empty/wrench@cartpole_msgs/EntityWrench@ignition.msgs.EntityWrench"
-    #     ],
-    #     output="screen",
-    # )
+    # Ign - ROS Bridge
+    bridge = Node(
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
+        arguments=[
+                # Clock (IGN -> ROS2)
+                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+                # Joint states (IGN -> ROS2)
+                '/world/empty/model/cartpole/joint_state'
+                + '@sensor_msgs/msg/JointState[ignition.msgs.Model',
+                '/model/cartpole/joint/beam_to_cart/cmd_force'
+                + '@std_msgs/msg/Float64@ignition.msgs.Double',
+        ],
+        remappings=[
+            ('/world/empty/model/cartpole/joint_state', 'joint_states'),
+            ('/model/cartpole/joint/beam_to_cart/cmd_force', 'cart_cmd_force'),
+        ],
+        output='screen'
+    )
 
-    return LaunchDescription([
-        SetEnvironmentVariable(name='IGN_GAZEBO_RESOURCE_PATH', value=model_path),
-        gazebo,
-        robot_state_publisher,
-        spawner,
-        # bridge,
-    ])
+    return LaunchDescription(
+        [
+            # Env variables
+            SetEnvironmentVariable(name='IGN_GAZEBO_RESOURCE_PATH', value=model_path),
+            # Nodes and Launches
+            gazebo,
+            spawn,
+            bridge,
+            robot_state_publisher,
+            rviz,
+        ]
+    )
